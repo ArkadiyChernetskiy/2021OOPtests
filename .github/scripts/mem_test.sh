9349b1e0b55c0bd94da7c5e1e05c3580d187be5a
arkadiy.chernetskiy <arkadiy.chernetskiy@phystech.edu>
2021-11-16 15:44:53 +0300

    little fix

--- .github/scripts/mem_test.sh
+++ .github/scripts/mem_test.sh
@@ -2,7 +2,7 @@
 
 script_path=./.github/scripts
 
-valgrind --log-file=./valgrind_log.txt ./a.out > out.txt
+valgrind --error-exitcode=3 --exit-on-first-error=yes --log-file=./valgrind_log.txt ./a.out > out.txt
 Total_memory=$(cat valgrind_log.txt | grep 'total heap usage' | awk -F ' ' '{print $9}' | sed 's/,//g')
 let Total_memory=$Total_memory-76800
 Total_allocs=$(cat valgrind_log.txt | grep 'total heap usage' | awk -F ' ' '{print $5}' | sed 's/,//g')
